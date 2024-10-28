83ee19b20b27c66209b0ee7c464a111bd6b4fb5d
PabloVasquez <vascor.pablo@gmail.com>
2024-10-27 21:05:04 -0300

    changing bd config from sqlite to postgres

--- backend_core/settings.py
+++ backend_core/settings.py
@@ -96,9 +96,14 @@ WSGI_APPLICATION = 'backend_core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gameworld-db',  
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  
+        'PORT': '5432',       
     }
 }
 

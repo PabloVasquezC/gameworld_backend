2758713d5ece5cef473524b0a43f1703c568ae4d
PabloVasquez <vascor.pablo@gmail.com>
2024-12-25 09:31:35 -0300

    implementing order models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
